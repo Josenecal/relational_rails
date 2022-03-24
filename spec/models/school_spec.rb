118d7228fc901b4b7101841e0d9d3ff4b4e8d817
Joseph Senecal <jasenecal2@gmail.com>
2022-03-24 14:13:50 -0600

    Test: basic model tests

---
+++ spec/models/school_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe School do
+
+  describe 'Validation' do
+
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :mailing_address }
+    it { should validate_presence_of :max_student_capacity }
+    it { should validate_presence_of :state_rating }
+    it { should validate_presence_of :is_public? }
+    it { should validate_presence_of :calendar_structure }
+    it { should validate_presence_of :grades_served }
+
+  end
+
+  describe 'Relationships' do
+
+    it { should have_many :staff_members }
+  end
+end
---
+++ spec/models/staff_member_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+RSpec.describe StaffMember do
+
+  describe 'Validation' do
+
+    it { should validate_presence_of :role }
+    it { should validate_presence_of :pay_rate }
+    it { should validate_presence_of :pay_type }
+    it { should validate_presence_of :qualified_instructor? }
+    it { should validate_presence_of :department }
+    it { should validate_presence_of :courses }
+
+  end
+
+  describe 'Relationships' do
+
+    it { should belong_to :school }
+  end
+end
