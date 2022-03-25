37ec7ef7daada5c0c0eee244dcef6de5006f261c
Joseph Senecal <jasenecal2@gmail.com>
2022-03-24 22:16:16 -0600

    Model: add school staff_member relationship, validations

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,11 @@
 class School < ApplicationRecord
+has_many:staff_members
 
+validates_presence_of:name
+validates_presence_of:mailing_address
+validates_presence_of:max_student_capacity
+validates_presence_of:state_rating
+validates_presence_of:is_public
+validates_presence_of:calendar_structure
+validates_presence_of:grades_served
 end
--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,3 +1,12 @@
-class StafMember < ApplicationRecord
+class StaffMember < ApplicationRecord
+  belongs_to:school
+
+  validates_presence_of:name
+  validates_presence_of:role
+  validates_presence_of:pay_rate
+  validates_presence_of:pay_type
+  validates_presence_of:department
+  validates_presence_of:courses
+  validates_presence_of:qualified_instructor
 
 end
