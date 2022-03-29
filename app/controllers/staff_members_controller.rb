30d1ecb16b8c54df01851b026cca85dda3ffa124
Joseph Senecal <jasenecal2@gmail.com>
2022-03-29 13:48:27 -0600

    Feat: update action with strong params

--- app/controllers/staff_members_controller.rb
+++ app/controllers/staff_members_controller.rb
@@ -12,4 +12,22 @@ class StaffMembersController < ApplicationController
     @staff_member = StaffMember.find(params[:id])
   end
 
+  def update
+    StaffMember.find(params[:id]).update(update_staff_member_params)
+    redirect_to "/staff_members/#{params[:id]}"
+  end
+
+  private
+
+   def update_staff_member_params
+     {
+       name: params[:name],
+       role: params[:role],
+       department: params[:department],
+       pay_rate: params[:pay_rate],
+       pay_type: params[:pay_type],
+       qualified_instructor: params[:qualified_instructor],
+       courses: params[:courses]
+     }
+
 end
