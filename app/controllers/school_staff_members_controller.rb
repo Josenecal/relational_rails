551de3264543105385169be5cad9ddb37a12936f
Joseph Senecal <jasenecal2@gmail.com>
2022-03-28 22:13:39 -0600

    Ref: update index instance variables to access school ID for link

--- app/controllers/school_staff_members_controller.rb
+++ app/controllers/school_staff_members_controller.rb
@@ -2,6 +2,6 @@ class SchoolStaffMembersController < ApplicationController
 
   def index
     @school_staff_members = School.find(params[:id]).staff_members
-    @school_name = School.find(params[:id]).name
+    @school = School.find(params[:id])
   end
 end
