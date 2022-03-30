3f384eb708fa0759db23f0aa7fd95ae45045cd8c
Joseph Senecal <jasenecal2@gmail.com>
2022-03-29 19:12:22 -0600

    Test: staff_member delete

--- spec/features/staff_members/show_spec.rb
+++ spec/features/staff_members/show_spec.rb
@@ -42,4 +42,12 @@ RSpec.describe "staff_members#show" do
     expect(page).to have_link(href: "/schools/")
     expect(page).to have_link(href: "/staff_members/")
   end
+
+  it 'has a link to delete the staff member' do
+    temp_staff_member = StaffMember.create!(name: "Bean, Mr.", role: "substitute teacher", department: "Math", courses: [], pay_rate: 1000000, pay_type: "salary", qualified_instructor: 1, active_employee: 1)
+    visit "staff_members/"
+    click_on "Delete Staff Member"
+    expect(current_path).to be("/staff_members/")
+    expect(page).not_to have_content(temp_staff_member.name)
+  end
 end
